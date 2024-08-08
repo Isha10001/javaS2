c9102f1cbaceae239278f7400e7f818072ee733c
Ishagupta <khushigupta200310@gmail.com>
2024-08-08 10:47:22 +0000

    conversion of datatypes

---
+++ jsfile/conversionOperation.js
@@ -0,0 +1,32 @@
+let score ="isha"// NaN (not a number)
+
+// const {score} = req.body
+
+
+// let score "33abc"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+
+  let valueInNumber = Number(score)
+  console.log(typeof valueInNumber);
+  console.log(valueInNumber);
+
+// "33" =>33
+// 33abc=> NaN
+// true=>1 ;false=>0
+
+let isLoggedIn = 1 
+// this value is true
+//  let booleanIsLoogedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoogedIn);
+
+let someNumber = 33
+let stringNumber = String (someNumber)
+console.log(stringNumber);
+
+console.log( typeof stringNumber);  // this is actual value of string
+
+ 
+  
