8e2f078b72f1c8e16c767a9b1cce2fbb17de5f70
Ishagupta <khushigupta200310@gmail.com>
2024-08-17 07:48:36 +0000

    string-update

--- jsfile/datatypes-summary.js
+++ jsfile/datatypes-summary.js
@@ -4,12 +4,17 @@
  Answer--2types Of categories -- 
 
  ---Primitive Data Types--  For ex- 
-7types--
+       7types--
   string,- const score =100
+
   Number- scoreValue = 200.7
+
    Boolean- isLoggedIn=false
+
    null,-outSideTemp=null (always empty)
+
     undefined,- let  userName;
+
     Symbol,-  const id= Symbol ('124') -- unique value
                const anotherId  = Symbol('124')
         console.log(id===anotherId);
@@ -27,17 +32,40 @@
 //Array-  
 const  heros = ["vivo","oppo","doga"];
 // Objects
-let myObj ={
-    name: ="isha",
-    age:20,
-}
+// let myObj ={
+//     name: ="isha",
+//     age:20,
+// }
 // Function
 const myFunction = function(){
-    console.log("hello world");
+//     console.log("hello world");
     
 }
-console.log( typeof myFunction);
+// console.log( typeof myFunction);
  
 
+
+
+// ++++++++++++++++++++++++++++Memory++++++++++++++//
+
+// stack(primitive)  ---- jab bhi koi value legai toh stack ke under do uska copy hi milta hai 
+// , heap(non Primitive ) -- esmai humsa refernce milta hai means (orignal value hi milta hai)
+
+let  mynameIsha ="happy"
+let anothername = mynameIsha
+anothername= "khushi"
+console.log(mynameIsha);
+console.log(anothername);
+
+  let userOne={
+    email:"user@gmail.com",
+    upi:"user@ybl"
+
+  }
+   let userTwo = userOne
+
+   userTwo.email ="happy@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
 
 
---
+++ jsfile/string.js
@@ -0,0 +1,43 @@
+const name ="isha"
+const repoCount = 50
+
+// console.log(name + repoCount+"value");  this is old method not use this 
+
+// new method for string use 
+
+console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+
+
+// string declared
+const gameName = new String ("isha-hc-nr")
+
+console.log(gameName[0]);
+console.log(gameName[7]);
+ console.log(gameName.length);
+ console.log(gameName.anchor);
+ console.log(gameName.padStart);
+ 
+ const newString= gameName.substring(0,4)
+ console.log(newString);
+
+ const anotherString = gameName.slice(-8,3)
+ console.log(anotherString);
+ 
+
+ const newStringOne ='  isha   '
+ console.log(newStringOne);
+ console.log(newStringOne.trim());
+
+ const url ="khushi123%20gmail.com"
+ console.log(url.replace('%20','-'));
+  
+console.log(url.includes('khushi'));
+console.log(url.includes('isha'));
+
+
+ 
+ console.log(gameName.split('-'));
+//  after use site mdn and 
+ 
+ 
+
